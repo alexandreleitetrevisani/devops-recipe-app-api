884c9bb081ffb3966b3e5eb1d04b0daa895e2e6c
alextrevis <leitetrevisani@gmail.com>
2026-06-30 21:14:14 -0300

    one more statement in iam.tf fot rds

--- infra/setup/iam.tf
+++ infra/setup/iam.tf
@@ -170,12 +170,12 @@ data "aws_iam_policy_document" "rds" {
     ]
     resources = ["*"]
   }
-  Statement {
-    effect: "Allow",
-    action: "rds:AddTagsToResource",
-    resource: "arn:aws:rds:us-east-1:436355585582:subgrp:raa-staging-main"
+  statement {
+    effect    = "Allow"
+    actions   = ["rds:AddTagsToResource"]
+    resources = ["arn:aws:rds:us-east-1:436355585582:subgrp:raa-staging-main"]
   }
-   
+
 }
 
 resource "aws_iam_policy" "rds" {
