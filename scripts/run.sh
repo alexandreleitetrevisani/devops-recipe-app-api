77899714ac9ead64b7151235417b063d18d90f05
alextrevis <leitetrevisani@gmail.com>
2026-07-04 05:34:56 -0300

    run.sh proxy and scripts/run.sh from api

--- scripts/run.sh
+++ scripts/run.sh
@@ -3,7 +3,12 @@
 set -e
 
 python manage.py wait_for_db
+
+# Criar a pasta local onde o utilizador django-user tem permissão
+mkdir -p /app/staticfiles
+
 python manage.py collectstatic --noinput
 python manage.py migrate
 
 uwsgi --socket :9000 --workers 4 --master --enable-threads --module app.wsgi
+
