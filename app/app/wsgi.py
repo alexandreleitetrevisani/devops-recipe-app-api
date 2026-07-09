4e4e9cdf49fb89c848fcf5f74d3fdb3ec910333c
alextrevis <leitetrevisani@gmail.com>
2026-07-08 21:18:05 -0300

    fix: format wsgi.py to comply with flake8

--- app/app/wsgi.py
+++ app/app/wsgi.py
@@ -7,23 +7,14 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 """
 
-#import os
-
-#from django.core.wsgi import get_wsgi_application
-
-#os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.settings')
-
-#application = get_wsgi_application()
-
 import os
 from django.core.wsgi import get_wsgi_application
-from whitenoise import WhiteNoise  # <--- ADICIONE ESTA LINHA
+from whitenoise import WhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.settings')
 
 application = get_wsgi_application()
 
-# Força o WhiteNoise a envelopar a aplicação e mapear a pasta /vol/web/static
 application = WhiteNoise(application, root='/vol/web/static')
 application.add_files('/vol/web/static', prefix='static/')
 
