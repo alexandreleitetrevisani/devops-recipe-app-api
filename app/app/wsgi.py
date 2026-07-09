a4f5e64d3e899b7a1e07568a979f1c00468de14f
alextrevis <leitetrevisani@gmail.com>
2026-07-08 21:04:38 -0300

    fix: activate whitenoise storage in settings

--- app/app/wsgi.py
+++ app/app/wsgi.py
@@ -7,10 +7,23 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 """
 
-import os
+#import os
 
+#from django.core.wsgi import get_wsgi_application
+
+#os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.settings')
+
+#application = get_wsgi_application()
+
+import os
 from django.core.wsgi import get_wsgi_application
+from whitenoise import WhiteNoise  # <--- ADICIONE ESTA LINHA
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.settings')
 
 application = get_wsgi_application()
+
+# Força o WhiteNoise a envelopar a aplicação e mapear a pasta /vol/web/static
+application = WhiteNoise(application, root='/vol/web/static')
+application.add_files('/vol/web/static', prefix='static/')
+
--- scripts/run.sh
+++ scripts/run.sh
@@ -10,7 +10,8 @@ mkdir -p /vol/web/static
 python manage.py collectstatic --noinput
 python manage.py migrate
 
-# uWSGI com mapeamento explícito e permissão de diretório seguro
-uwsgi --http :9000 --workers 4 --master --enable-threads --module app.wsgi --static-map /static=/vol/web/static --static-safe /vol/web/static
+uwsgi --http :9000 --workers 4 --master --enable-threads --module app.wsgi
+
+
 
 
