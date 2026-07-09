6756df8c791ff23d33952d5473ca728827279e51
alextrevis <leitetrevisani@gmail.com>
2026-07-08 21:25:05 -0300

    fix: remove blank line at end of wsgi.py

--- app/app/wsgi.py
+++ app/app/wsgi.py
@@ -1,12 +1,3 @@
-"""
-WSGI config for app project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
-"""
-
 import os
 from django.core.wsgi import get_wsgi_application
 from whitenoise import WhiteNoise
@@ -17,4 +8,3 @@ application = get_wsgi_application()
 
 application = WhiteNoise(application, root='/vol/web/static')
 application.add_files('/vol/web/static', prefix='static/')
-
